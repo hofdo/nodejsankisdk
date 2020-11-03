4d22d6436984f1f254093d247679db5b534a82b5
hofmd2 <hofmd@bfh.ch>
2020-11-03 21:59:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-//let noble = require('noble-uwp');
+//let noble = require('noble');
 let noble = require('noble-uwp');
 let util = require('util')
 
 let vehicles = [];
 let i = 0;
 
-noble.on('stateChange', function(state) {
-    if (state === 'poweredOn') {
-        noble.startScanning(['be15beef6186407e83810bd89c4d8df4']);
-    } else {
-        noble.stopScanning();
-    }
-});
+noble.startScanning(['be15beef6186407e83810bd89c4d8df4']);
 
-noble.on('discover', function(device) {
-        vehicles = {
-        'vehicle_id': device.address,
+setTimeout(function (){
+    noble.stopScanning();
+}, 2000);
+
+function connect(vehicle) {
+    vehicle.connect(function(error) {
+        vehicle.discoverSomeServicesAndCharacteristics(
+            ["be15beef6186407e83810bd89c4d8df4"],
+            ["be15bee06186407e83810bd89c4d8df4", "be15bee16186407e83810bd89c4d8df4"],
+            function(error, services, characteristics) {
+                //  console.log("!!!!!!!!!!!!!!!!!");
+                //  console.log(characteristics);
+                //  console.log("!!!!!!!!!!!!!!!!!");
+                vehicle.reader = characteristics[1];//.find(x => !x.properties.includes("write"));
+                vehicle.writer = characteristics[0];//.find(x => x.properties.includes("write"));
+
+                vehicle.reader.notify(true);
+                vehicle.reader.on('data', function(data, isNotification) {
+                    console.log(util.format("%s;%s\n", vehicle.id, data.toString("hex")));
+                });
+                console.log("connect success");
+            }
+        );
+    });
+}
+
+function setSpeed(device) {
+    let message = new Buffer(7);
+    message.writeUInt8(0x06, 0);
+    message.writeUInt8(0x24, 1);
+    message.writeInt16LE(500, 2);
+    message.writeInt16LE(0, 4);
+    device.writer.write(message, true);
+}
+
+function discover (device) {
+    vehicles = {
+        'vehicle_id': device.id,
+        'vehicle_uuid': device.advertisement.manufacturerData.toString('hex'),
         'peripheral': device
     }
-    console.log(util.format("SCAN;%s;%s\n",
-        device.id,
-        device.advertisement.manufacturerData.toString('hex')));
-});
+    //console.log(util.format("SCAN;%s;%s\n", device.id, device.advertisement.manufacturerData.toString('hex')));
+    connect(noble._peripherals[device.id]);
+    setSpeed(noble._peripherals[device.id]);
+}
+
+noble.on('discover', discover);
+
 
 
