5220f2ce576325765675cf2632954a9b86bd2036
hofmd2 <hofdm2@bfh.ch>
2020-11-30 18:03:22 +0100

    Added Response code

--- controller.js
+++ controller.js
@@ -392,7 +392,7 @@ function uTurn(device_id) {
     message.writeUInt8(0x03, 0);
     message.writeUInt8(0x32, 1); // ANKI_VEHICLE_MSG_C2V_TURN_180
     message.writeUInt8(0x01, 2);
-    message.writeUInt8(0x00, 3);
+    message.writeUInt8(0x01, 3);
     vehicles[device_id]['writer'].write(message);
 }
 
