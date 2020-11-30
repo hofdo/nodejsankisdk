429b02134619c0ad1f4c13ff758dab24a9d36ef3
hofmd2 <hofdm2@bfh.ch>
2020-12-01 00:02:19 +0100

    Added Response code

--- controller.js
+++ controller.js
@@ -264,7 +264,7 @@ function dataListener(data, isNotification, vehicle){
             //  ANKI_VEHICLE_MSG_V2C_LOCALIZATION_INTERSECTION_UPDATE
             console.log(vehicle.id + "Message_id: " + messageID + " road_piece_idx: " + data.readInt8(2) + " offset: "
                 + data.readFloatLE(3) + " intersection_code: " + data.readUInt8(4) + " is_exiting: " + data.readUInt8(5)
-                + "transition_bar: " + data.readUInt16LE(6) + "insection_code: " + data.readUInt16LE(7));
+                + " mm_transition_bar: " + data.readUInt16LE(6) + " mm_insection_code: " + data.readUInt16LE(7));
             break;
         case 43:
             // ANKI_VEHICLE_MSG_V2C_VEHICLE_DELOCALIZED
